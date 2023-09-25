dcc2fb427fd6d23ae945a7b270d88540a5c25466
LW7373 <96071346+LW7373@users.noreply.github.com>
2023-09-24 21:10:58 -0400

    Update and rename Box.java to PackBox.java

--- PackBox.java
+++ PackBox.java
@@ -21,6 +21,9 @@ public class PackBox{
     private boolean isShipped;
     private Sausage[] sausageArray;
 
+    public PackBox(){
+
+    }
     public PackBox(String material, double length, double width, double height, int numSausageLinks, boolean isShipped, Sausage[] sausageArray) {
         this.material = material;
         this.length = length;
@@ -30,20 +33,6 @@ public class PackBox{
         this.isShipped = isShipped;
         this.sausageArray = sausageArray;
     }
-
-    //public Box(){
-    //    for (int a = 0; a < 24; a++){
-    //        sausageArray[a] = new Sausage();
-    //    }
-    //}
-
-
-
-
-
-
-
-
 
     // GETTERS AND SETTERS
     public String getMaterial(){
@@ -107,48 +96,60 @@ public class PackBox{
 
     
     // CRUD
-    private void AddSausage(Sausage newSausage){        
+    public void AddSausage(Sausage newSausage){        
         int arrayLen =  this.sausageArray.length;
-        Sausage[] sausageArrayNew = new Sausage(arrayLen + 1);
+        Sausage[] sausageArrayNew = new Sausage[arrayLen + 1];
         System.arraycopy(sausageArray, 0, sausageArrayNew, 0, arrayLen);
-        sausageArrayNew[arrayLen + 1] = newSausage; 
+        sausageArrayNew[arrayLen] = newSausage; 
         this.setSausageArray(sausageArrayNew);
-        for (int i = 0; i < arrayLen + 1){
-           system.print(sausageArray[i]);
-        }
     }
 
-    private Sausage ReturnSausage (){
-        // return sausageArray[a][b];
-        // return new Sausage();
+    public void ReadAllSausages(){
         int arrayLen = this.sausageArray.length;
-        for (int i = 0; i < arrayLen + 1){
-           system.print(sausageArray[i]);
+        for (int i = 0; i < arrayLen; i++){
+            System.out.println("Sausage " + (i + 1) + ": " + sausageArray[i]);
         }
     }
 
-    private void ChangeSausage(int arrayIndex, Sausage newSausage){
-        this.sausageArray[arrayIndex] = newSausage;
+    public void ReadOneSausage(int selectedSausage){
+        System.out.println("Sausage " + (selectedSausage) + ": " + sausageArray[selectedSausage - 1]);
+    }
+
+    public void ChangeSausage(int selectedSausage, Sausage newSausage){
+        this.sausageArray[selectedSausage - 1] = newSausage;
         this.setSausageArray(sausageArray);
-        for (int i = 0; i < sausageArray.length){
-            system.print(sausageArray[i]);
-        }
+        System.out.println("Updated Sausage " + selectedSausage); // change for user input
+        // for (int i = 0; i < sausageArray.length; i++){
+        //     System.out.println("Sausage " + (i + 1) + ": " + sausageArray[i]);
+        // }
     }
 
-    private void DeleteSausage(int arrayIndex){
-        //sausageArray[a][b] = new Sausage(); 
+    public void DeleteSausage(int selectedSausage){
         int arrayLen =  this.sausageArray.length;
-        Sausage[] sausageArrayNew = new Sausage(arrayLen - 1);        
-        int j = arrayIndex;
-        for(int i= 0, k = 0; i< arrayLen; i++){
-            if(i != j){
+        Sausage[] sausageArrayNew = new Sausage[arrayLen - 1];
+        for(int i = 0, k = 0; i < arrayLen; i++){
+            if(i != (selectedSausage - 1)){
                 sausageArrayNew[k] = this.sausageArray[i];
                 k++;
             }
         }
         this.setSausageArray(sausageArrayNew);
-        for (int i = 0; i < arrayLen + 1){
-            system.print(sausageArray[i]);
+        System.out.println("Deleted Sausage " + selectedSausage);
+        for (int i = 0; i < arrayLen - 1; i++){
+            System.out.println("Sausage " + (i + 1) + ": " + sausageArray[i]);
         }
+    }
+
+    public String toString(){
+        String s = "PackBox Stats\n";
+        s += "============================\n";
+        s += "Material: " + this.material + "\n";
+        s += "Length: " + this.length + "\n";
+        s += "Width: " + this.width + "\n";
+        s += "Height: " + this.height + "\n";
+        s += "NumSausageLinks: " + this.numSausageLinks + "\n";
+        s += "Shipped? " + this.isShipped;
+
+        return s;
     }
 }
